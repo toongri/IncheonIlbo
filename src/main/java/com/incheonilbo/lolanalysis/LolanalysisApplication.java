7007dc4ed19fc4b33559c9b1d872d0d9fc4b39ca
Toong_laptop <jhkim03284@gmail.com>
2021-12-05 23:31:11 +0900

    0th commit : set

---
+++ src/main/java/com/incheonilbo/lolanalysis/LolanalysisApplication.java
@@ -0,0 +1,13 @@
+package com.incheonilbo.lolanalysis;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LolanalysisApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LolanalysisApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/incheonilbo/lolanalysis/LolanalysisApplicationTests.java
@@ -0,0 +1,13 @@
+package com.incheonilbo.lolanalysis;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LolanalysisApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
