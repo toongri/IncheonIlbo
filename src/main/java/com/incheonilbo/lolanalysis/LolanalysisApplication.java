4f159b03f9d1480c42703be0a2b8683a0504a3d8
Toong_laptop <jhkim03284@gmail.com>
2022-01-25 23:40:42 +0900

    11th commit : 서버용 빌드완성

--- src/main/java/com/incheonilbo/lolanalysis/LolanalysisApplication.java
+++ src/main/java/com/incheonilbo/lolanalysis/LolanalysisApplication.java
@@ -2,10 +2,16 @@ package com.incheonilbo.lolanalysis;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class LolanalysisApplication {
+public class LolanalysisApplication extends SpringBootServletInitializer {
 
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(LolanalysisApplication.class);
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(LolanalysisApplication.class, args);
 	}
